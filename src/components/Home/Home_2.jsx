88ac4307cf86cb453ae7379053d4119622516431
Nabeel Nasir <69030093+nabeelnasir9@users.noreply.github.com>
2023-12-21 05:52:50 +0500

    home2-page fixes and home1 pages fixes

--- src/components/Home/Home_2.jsx
+++ src/components/Home/Home_2.jsx
@@ -1,11 +1,26 @@
 import '../Home/Home_2.css';
+import img1 from '../../assets/Home/img1.png';
+import img2 from '../../assets/Home/img2.png';
+import img3 from '../../assets/Home/img3.png';
 const Home_2 = () => {
   return (
     <div className='Home2_wrapper'>
-        <button className='home_2_button'>
+       <button className='home_2_button'>
         LEADING CUTTING EDGE SOLUTION FOR INDUSTRY
-
-</button>
+       </button>
+       <div className='home_2_texts'>
+        <div className='home_2_texts_1'>
+         <span style={{color:'#f19100'}}>  3 Steps </span> Solution for you to grow and
+        </div>
+        <div className='home_2_texts_2'>
+           expand your business with help of us.
+        </div>
+       </div>
+       <div className='home_2_imgs'>
+        <img src={img1} alt="" className='home_2_img-1'/>
+        <img src={img2} alt="" className='home_2_img-2'/>
+        <img src={img3} alt="" className='home_2_img-3'/>
+       </div>
     </div>
   )
 }
